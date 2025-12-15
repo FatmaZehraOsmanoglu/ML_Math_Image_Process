c33d4a47ee206fad1ddc89348312e270d9cc6d23
FatmaZehraOsmanoglu <fatmazehraosmanoglu@gmail.com>
2025-12-15 10:17:50 +0300

    ML math image process performans odevi guncellemesi

---
+++ ImageProcessingWinForms/ImageProcessingWinForms/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace ImageProcessingWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnGray = new System.Windows.Forms.Button();
+            this.btnTers = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSepya = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(82, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(523, 331);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(741, 51);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(524, 331);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(232, 486);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(181, 69);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "ResimYukle";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnGray
+            // 
+            this.btnGray.Location = new System.Drawing.Point(232, 628);
+            this.btnGray.Name = "btnGray";
+            this.btnGray.Size = new System.Drawing.Size(181, 62);
+            this.btnGray.TabIndex = 3;
+            this.btnGray.Text = "Gri Yap ";
+            this.btnGray.UseVisualStyleBackColor = true;
+            this.btnGray.Click += new System.EventHandler(this.btnGray_Click);
+            // 
+            // btnTers
+            // 
+            this.btnTers.Location = new System.Drawing.Point(565, 486);
+            this.btnTers.Name = "btnTers";
+            this.btnTers.Size = new System.Drawing.Size(190, 69);
+            this.btnTers.TabIndex = 4;
+            this.btnTers.Text = "Ters Renge Cevir";
+            this.btnTers.UseVisualStyleBackColor = true;
+            this.btnTers.Click += new System.EventHandler(this.btnTers_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(875, 486);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(190, 62);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Kaydet";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(874, 621);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(191, 69);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Sifirla";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnSepya
+            // 
+            this.btnSepya.Location = new System.Drawing.Point(565, 628);
+            this.btnSepya.Name = "btnSepya";
+            this.btnSepya.Size = new System.Drawing.Size(190, 62);
+            this.btnSepya.TabIndex = 7;
+            this.btnSepya.Text = "Sepya Filtresi";
+            this.btnSepya.UseVisualStyleBackColor = true;
+            this.btnSepya.Click += new System.EventHandler(this.btnSepya_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1361, 828);
+            this.Controls.Add(this.btnSepya);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnTers);
+            this.Controls.Add(this.btnGray);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnGray;
+        private System.Windows.Forms.Button btnTers;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSepya;
+    }
+}
+
---
+++ ImageProcessingWinForms/ImageProcessingWinForms/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ImageProcessingWinForms
+{
+    public partial class Form1 : Form
+    {
+        Bitmap originalImage;   // Orijinal resim
+        Bitmap editedImage;     // İşlenen resim
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // ===================== RESİM YÜKLE =====================
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Resim Dosyaları|*.jpg;*.png;*.jpeg;*.bmp";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                originalImage = new Bitmap(ofd.FileName);
+                editedImage = (Bitmap)originalImage.Clone();
+
+                pictureBox1.Image = originalImage;
+                pictureBox2.Image = editedImage;
+            }
+        }
+
+        // ===================== GRİYE ÇEVİR =====================
+        private void btnGray_Click(object sender, EventArgs e)
+        {
+            if (editedImage == null) return;
+
+            Bitmap temp = new Bitmap(editedImage.Width, editedImage.Height);
+
+            for (int x = 0; x < editedImage.Width; x++)
+            {
+                for (int y = 0; y < editedImage.Height; y++)
+                {
+                    Color c = editedImage.GetPixel(x, y);
+                    int gri = (c.R + c.G + c.B) / 3;
+                    temp.SetPixel(x, y, Color.FromArgb(gri, gri, gri));
+                }
+            }
+
+            editedImage = temp;
+            pictureBox2.Image = editedImage;
+        }
+
+        // ===================== TERS RENK =====================
+        private void btnTers_Click(object sender, EventArgs e)
+        {
+            if (editedImage == null) return;
+
+            Bitmap temp = new Bitmap(editedImage.Width, editedImage.Height);
+
+            for (int x = 0; x < editedImage.Width; x++)
+            {
+                for (int y = 0; y < editedImage.Height; y++)
+                {
+                    Color c = editedImage.GetPixel(x, y);
+                    temp.SetPixel(x, y, Color.FromArgb(
+                        255 - c.R,
+                        255 - c.G,
+                        255 - c.B
+                    ));
+                }
+            }
+
+            editedImage = temp;
+            pictureBox2.Image = editedImage;
+        }
+
+
+        // ===================== 🔥 SEPYA FİLTRESİ =====================
+        private void btnSepya_Click_1(object sender, EventArgs e)
+        {
+            if (editedImage == null) return;
+
+            Bitmap temp = new Bitmap(editedImage.Width, editedImage.Height);
+
+            for (int x = 0; x < editedImage.Width; x++)
+            {
+                for (int y = 0; y < editedImage.Height; y++)
+                {
+                    Color c = editedImage.GetPixel(x, y);
+
+                    int r = (int)(c.R * 0.393 + c.G * 0.769 + c.B * 0.189);
+                    int g = (int)(c.R * 0.349 + c.G * 0.686 + c.B * 0.168);
+                    int b = (int)(c.R * 0.272 + c.G * 0.534 + c.B * 0.131);
+
+                    // Değerler 255'ten büyük olursa 255 yap (renk taşmasını engeller)
+                    r = (r > 255) ? 255 : r;
+                    g = (g > 255) ? 255 : g;
+                    b = (b > 255) ? 255 : b;
+
+                    temp.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+
+            editedImage = temp;
+            pictureBox2.Image = editedImage;
+        }
+
+
+        // ===================== RESET =====================
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            if (originalImage == null) return;
+
+            editedImage = (Bitmap)originalImage.Clone();
+            pictureBox2.Image = editedImage;
+        }
+
+        // ===================== KAYDET =====================
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image == null) return;
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "PNG|*.png|JPEG|*.jpg|Bitmap|*.bmp";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox2.Image.Save(sfd.FileName);
+                MessageBox.Show("Resim kaydedildi.", "Başarılı");
+            }
+        }
+    }
+}
